6b7d747814695246916d3b3e498ee5ce8513d721
Timothee Carayol <timothee.carayol@gmail.com>
2021-12-08 10:11:44 +0100

    refactored day 8

--- 08/08.py
+++ 08/08.py
@@ -1,5 +1,5 @@
 import numpy as np
-from import Counter
+from collections import Counter
 
 def parse_input(file):
     lines = open(file,'r').readlines()
@@ -17,14 +17,65 @@ def part_1_sum(output_array):
         np.sum(output_length_array == 7, axis=(0, 1))
     )
 
-def decoder(sig_row):
+def build_decoder(sig_row):
+    """Builds the decoder dict for a given (1,10) signal row."""
     sig_len = np.vectorize(len)(sig_row)
-    decoded = {}
-    decoded['a'] = list(set(sig_row[sig_len == 3][0]) - set(sig_row[sig_len == 2][0]))[0]
-    fivers = Counter(''.join(sig_row[sig_len == 5])))
-    return a
+    decoder = {}
+    count_all = Counter(''.join(sig_row))
+    count_no_4 = Counter(''.join(sig_row[sig_len!=4]))
+    for k in "abcdefg":
+        to_match = (count_all[k], count_no_4[k])
+        if to_match == (8, 8):
+            decoder[k] = "a"
+        elif to_match == (6, 5):
+            decoder[k] = "b"
+        elif to_match == (8, 7):
+            decoder[k] = "c"
+        elif to_match == (7, 6):
+            decoder[k] = "d"
+        elif to_match == (4, 4):
+            decoder[k] = "e"
+        elif to_match == (9, 8):
+            decoder[k] = "f"
+        elif to_match == (7, 7):
+            decoder[k] = "g"
+    return(decoder)
+
+def decode(output, decoder):
+    """Given the (1,4) input row and the decoder, returns the corresponding row integer."""
+    decoded_digits = ""
+    for digit in output:
+        decoded_digit_segments = set()
+        for char in digit:
+            decoded_digit_segments.add(decoder[char])
+        if decoded_digit_segments == set("abcefg"):
+            decoded_digits += "0"
+        elif decoded_digit_segments == set("cf"):
+            decoded_digits += "1"
+        elif decoded_digit_segments == set("acdeg"):
+            decoded_digits += "2"
+        elif decoded_digit_segments == set("acdfg"):
+            decoded_digits += "3"
+        elif decoded_digit_segments == set("bcdf"):
+            decoded_digits += "4"
+        elif decoded_digit_segments == set("abdfg"):
+            decoded_digits += "5"
+        elif decoded_digit_segments == set("abdefg"):
+            decoded_digits += "6"
+        elif decoded_digit_segments == set("acf"):
+            decoded_digits += "7"
+        elif decoded_digit_segments == set("abcdefg"):
+            decoded_digits += "8"
+        elif decoded_digit_segments == set("abcdfg"):
+            decoded_digits += "9"
+    return(int(decoded_digits))
+
 
 if __name__ == "__main__":
     signals_array, output_array= parse_input("08/input")
-    print(part_1_sum(output_array))
-    decoder(signals_array[0])
+    print(part_1_sum(output_array)) # part 1
+    decoded_output = []
+    for i in range(signals_array.shape[0]):
+        decoder = build_decoder(signals_array[i,:])
+        decoded_output += [decode(output_array[i], decoder)]
+    print(sum(decoded_output)) # part 2
