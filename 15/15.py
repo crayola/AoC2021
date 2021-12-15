a49f6f093651b19619fe828cbd46de44620c938a
Timothée Carayol <timothee.carayol@gmail.com>
2021-12-15 21:58:31 +0100

    further refactor day 15

--- 15/15.py
+++ 15/15.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-def update_costs(visited: np.ndarray, current: tuple, costs: np.ndarray, risks: np.ndarray):
+def update_costs(visited: np.ndarray, costs: np.ndarray, risks: np.ndarray):
     """Update costs around current position"""
+    current = np.unravel_index(np.argmin(costs + visited), costs.shape)
     visited[current] = np.inf
     for i in range(max(0, current[0]-1), min(costs.shape[0], current[0]+2)):
         for j in range(max(0, current[1]-1), min(costs.shape[1], current[1]+2)):
             if (i == current[0]) != (j == current[1]): # xor
                 costs[i, j] = min(costs[i, j], costs[current] + risks[i, j])
-    current = np.unravel_index(np.argmin(costs + visited), costs.shape)
-    return (visited, current, costs, risks)
+    return (visited, costs, risks)
 
 def build_complete_risk_array(r):
     """Provides the input for part 2"""
@@ -23,12 +23,11 @@ def build_complete_risk_array(r):
     return full_risks
 
 def find_shortest_path(risks: np.ndarray):
-    costs = np.full_like(risks, np.inf)
-    visited = np.zeros_like(risks) 
+    costs = np.full_like(risks, np.inf, dtype=float)
+    visited = np.zeros_like(risks, dtype=float)
     costs[0,0] = 0
-    current = (0,0)
     while not visited[-1, -1]:
-        visited, current, costs, _ = update_costs(visited, current, costs, risks)
+        visited, costs, _ = update_costs(visited, costs, risks)
     return costs[-1, -1]
 
 if __name__ == "__main__":
@@ -36,5 +35,3 @@ if __name__ == "__main__":
     print("Part 1:", find_shortest_path(risks_1))
     risks_2 = build_complete_risk_array(risks_1)
     print("Part 2:", find_shortest_path(risks_2))
-    
-
