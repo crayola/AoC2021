0756340c2fada1cefe7a88b3f5be64cd582a8ae0
Timothee Carayol <timothee.carayol@gmail.com>
2021-12-17 11:48:47 +0100

    Day 17

--- 17/17.py
+++ 17/17.py
@@ -1,26 +1,20 @@
 def meet_target(velocity, target):
-    x = 0
-    y = 0
-    step = 0
+    x, y = (0, 0)
     while x <= target[0][1] and y >= target[1][0]:
-        step += 1
-        print (step, x, y)
         x += velocity[0]
         y += velocity[1]
         velocity = (velocity[0] - (velocity[0] > 0), velocity[1] - 1)
         if (x >= target[0][0] and x <= target[0][1] and 
             y >= target[1][0] and y <= target [1][1]):
-            return "Hit!"
-    return "No hit :-("
-
+            return True
+    return False
 
 if __name__ == '__main__':
-    target = [(88, 125), (-157, -103)]
-    #x=88..125, y=-157..-103
-
-    #final_x = 1 + 2 + .. + vel_x
-    #= (vel_x) * (vel_x + 1) / 2
-
-    #vel_x = 
-
-    print(meet_target((15, 156), target))
+    target = [(88, 125), (-157, -103)] # my input
+    print("Part 1:", target[1][0] * (target[1][0] + 1) // 2)
+    hits = []
+    for x in range(target[0][1] + 1):
+        for y in range(target[1][0] - 1, -target[1][0]):
+            if meet_target((x,y), target):
+                hits += [(x,y)]
+    print("Part 2:", len(hits))
